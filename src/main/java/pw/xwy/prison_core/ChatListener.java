cba57a88a3210ff140ebfce02aa67a4bb71b391a
reeve567 <18086052+reeve567@users.noreply.github.com>
2018-07-14 16:54:37 -0400

    up

--- src/main/java/pw/xwy/prison_core/ChatListener.java
+++ src/main/java/pw/xwy/prison_core/ChatListener.java
@@ -12,12 +12,11 @@ public class ChatListener implements Listener {
 	
 	@EventHandler
 	public void onChat(AsyncPlayerChatEvent e) {
+		e.setCancelled(true);
 		String message = e.getMessage();
 		if (message.contains("hello") && message.contains("billy")) {
-			e.setCancelled(true);
 			e.getPlayer().sendMessage(prefix + "Hello Im Billy your personal assistant and prostitute. Ask any questions that start with a what and address me somewhere in the question and i will answer");
 		} else if (message.contains("what") && message.contains("billy")) {
-			e.setCancelled(true);
 			e.getPlayer().sendMessage(prefix + "A dolphin can have up to 8 orgasms in 10 mins");
 		} else {
 			Bukkit.broadcastMessage("§7[Noob] " + e.getPlayer().getName() + " » " + e.getMessage());
